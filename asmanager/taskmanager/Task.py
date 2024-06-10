d14d489ee2d07960e61e117b97b718cb3e527289
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-06-09 21:10:33 -0400

    init task

--- asmanager/taskmanager/Task.py
+++ asmanager/taskmanager/Task.py
@@ -1,9 +1,25 @@
+from asmanager.statemanager.StateManager import StateManager, State
+from asmanager.environment.Environment import Environment
+from asmanager.actions.Actions import Actions
+
 class Task:
     def __init__(
         self,
         task: str,
+        state: State = None,
+        environment: Environment = None,
+        actions: Actions = None
     ):
         self.task = task
+
+        self.state_manager = StateManager(state) if state is not None \
+            else StateManager()
+        
+        self.environment = environment if environment is not None \
+            else Environment()
+        
+        self.actions = actions if actions is not None \
+            else Actions()
 
     def __str__(self):
         return self.task
